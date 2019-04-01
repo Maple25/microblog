d167234d3e9b631a72d662b96f6115791ba25fb6
MAPLE <845421209@qq.com>
2019-04-02 00:31:59 +0800

    弃用了获取用户名的方法 直接从传入的参数获取

--- src/com/maple/www/dao/AddMicroblog.java
+++ src/com/maple/www/dao/AddMicroblog.java
@@ -21,7 +21,7 @@ public class AddMicroblog {
         String sql="INSERT INTO microblog VALUES(null,?,?,?,?,?,?)";
         PreparedStatement pstmt=con.prepareStatement(sql);
         pstmt.setInt(1,mb.getSenderId());//后面要怎么修改成自动获取用户的ID
-        pstmt.setString(2,getSenderName(mb.getSenderId()));//再建一个类 通过发送者ID获取发送者用户名
+        pstmt.setString(2,mb.getSenderName());//再建一个类 通过发送者ID获取发送者用户名-----
         pstmt.setString(3,mb.getMbCategory());//3-30再建一个类getMicroblogCategory 获得类别 等能搞图形化再看看这部分怎么搞 //之前不成功是因为“后台代码通过jdbc写sql将数据传入数据库”没设置 参考https://blog.csdn.net/lsr40/article/details/78736855
         pstmt.setString(4,mb.getMbText());
         pstmt.setInt(5,mb.getLiked());
